e1305952c9615523d7e530cbf0620c864a818319
Katelyn Mulgrew <celloqueenie@gmail.com>
2015-12-03 22:02:39 -0500

    working client side form validation

--- littleMermaid/public/javascripts/clientSideFormValidation.js
+++ littleMermaid/public/javascripts/clientSideFormValidation.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
-	//check if this works
-	$('#link').on('input', function() {
+	//check if this works 
+	/*$('#link').on('input', function() {
 					var input=$(this);
 					if (input.val().substring(0,4)=='www.'){input.val('http://www.'+input.val().substring(4));}
 					var re = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
 					var is_url=re.test(input.val());
 					if(is_url){}
 					else{alert("Invalid Link!");}
+				});*/
+	//check if this works
+	$('#link').on('input', function() {
+					var input=$(this);
+					var re = /^((https|http)?:\/\/)/;
+					var is_url=re.test(input.val());
+					div = document.querySelector('#aboutLink');
+					if(is_url){
+						div.innerText="Valid Link";
+					}
+					else{
+						div.innerText="Invalid Link!";
+						//alert("Invalid Link!");
+					}
 				});
 
 });
